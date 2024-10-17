12319400e5fd97c6719eee91864839b20a032ed6
XiaoFan <xiaofan@lizhi.fm>
2024-10-17 12:00:49 +0800

    feat: main

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729136600',resources: [
+    cacheName: 'chirpy-1729137574',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
