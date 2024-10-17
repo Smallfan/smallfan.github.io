6b6647fc6e428c355d6949578e5f5667f3eec8e6
Smallfan <CoderSmallfan@qq.com>
2024-10-17 23:28:48 +0800

    feat: sdsdsd

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729164431',resources: [
+    cacheName: 'chirpy-1729175467',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
